32bd14d1bfcb9b7b2cd34784e5209e643894ba2c
Gustavo Rabelo <gurabelo09@gmail.com>
2024-10-04 18:36:06 -0300

    add swagger config

---
+++ src/main/java/com/odontoprev/byterisk/configs/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.odontoprev.byterisk.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ByteRisk API")
+                        .version("1.0")
+                        .description("API para a aplicação ByteRisk da OdontoPrev, focadoem redução de sinistros odontológicos.")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
+
