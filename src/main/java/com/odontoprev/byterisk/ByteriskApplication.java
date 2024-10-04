d06ad79104f34bd33e90d78321d7512db2232afb
Gustavo Rabelo <gurabelo09@gmail.com>
2024-10-04 18:16:05 -0300

    starting project

---
+++ src/main/java/com/odontoprev/byterisk/ByteriskApplication.java
@@ -0,0 +1,13 @@
+package com.odontoprev.byterisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteriskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteriskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odontoprev/byterisk/ByteriskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odontoprev.byterisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteriskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
