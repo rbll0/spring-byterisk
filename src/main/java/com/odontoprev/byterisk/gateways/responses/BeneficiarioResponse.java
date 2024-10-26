4486a53ffb44b2e6dbd658e104d661e864eca57c
Gustavo Rabelo <gurabelo09@gmail.com>
2024-10-26 17:34:06 -0300

    add hateoas: beneficiario response

--- src/main/java/com/odontoprev/byterisk/gateways/responses/BeneficiarioResponse.java
+++ src/main/java/com/odontoprev/byterisk/gateways/responses/BeneficiarioResponse.java
@@ -2,13 +2,15 @@ package com.odontoprev.byterisk.gateways.responses;
 
 import com.odontoprev.byterisk.domains.enums.Ranking;
 import lombok.Data;
+import oracle.jdbc.driver.Representation;
+import org.springframework.hateoas.RepresentationModel;
 
 /**
  * Classe que representa a resposta com os dados de um beneficiário. Usada para enviar as informações
  * do beneficiário nas respostas da API.
  */
 @Data
-public class BeneficiarioResponse {
+public class BeneficiarioResponse extends RepresentationModel<BeneficiarioResponse> {
 
     private Long idBeneficiario;
     private String nome;
